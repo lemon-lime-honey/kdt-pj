5b11f621414e4d8cf98fcf0aed8c24e1f23986d5
lemon-lime-honey <bs0308@g.skku.edu>
2023-04-07 17:47:40 +0900

    Revise Django 02

--- Django_02/accounts/forms.py
+++ Django_02/accounts/forms.py
@@ -50,6 +50,7 @@ class CustomUserCreationForm(UserCreationForm):
             attrs={
                 'type':'date',
                 'class': 'form-control',
+                'placeholder': '생년월일'
             }
         ),
     )
@@ -74,7 +75,7 @@ class CustomUserCreationForm(UserCreationForm):
     
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2',)
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'birthday')
 
 
 class CustomUserChangeForm(UserChangeForm):
